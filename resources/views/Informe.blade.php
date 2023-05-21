de3e546755fb8f3343ec926c7971c85d7e9e039a
jhorman_montoya <jhormaparaelcreador@gmail.com>
2023-05-20 23:19:20 -0500

    creacion de modelo

--- resources/views/Informe.blade.php
+++ resources/views/Informe.blade.php
@@ -9,7 +9,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    
+                    <div class="container">
+                        <thead>
+                            <tr>
+      <th scope="col">Name</th>
+      <th scope="col">last name</th>
+      <th scope="col">Nombre de la carrera</th>
+      <th scope="col">Numero</th>
+      <th scope="col">email</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($estudiantes as $estudiante)
+    <tr>
+      <td>{{$estudiante->name}}</td>
+      <td>{{$estudiante->last_name}}</td>
+      <td>{{$estudiante->carrera->Nom_Car}}</td>
+      <td>{{$estudiante->Num}}</td>
+      <td>{{$estudiante->email}}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+                    
+                    
+                    </div>
                 </div>
             </div>
         </div>
